22156a52de5e53606bda4e503d80991a17289d1a
luthfi rezkiansyah ramadhani <daniramadh47@gmail.com>
2025-03-23 18:11:16 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TpMod6_103022300147
+{
+    class SayTubeVideo
+    {
+        private string title;
+        private int viewCount;
+
+        public SayTubeVideo(string t)
+        {
+            title = t;
+            viewCount = 0;
+        }
+
+        public void IncreasePlayCount(int n, Action printVideoDetails)
+        {
+            viewCount += n;
+            printVideoDetails();
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {viewCount}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayTubeVideo video = new SayTubeVideo("Belajar C# Dasar");
+            video.PrintVideoDetails();
+            video.IncreasePlayCount(100, video.PrintVideoDetails);
+        }
+    }
+}
